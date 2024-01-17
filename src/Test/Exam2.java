16910fdd5bac269a6f837201f0cc85e60d2a7af2
Gubong_home <QAZ3915@NAVER.COM>
2024-01-17 22:36:21 +0900

    test 2

--- src/Test/Exam2.java
+++ src/Test/Exam2.java
@@ -1,9 +1,9 @@
 package Test;
 
-public class Exam {
+public class Exam2 {
 
 	public static void main(String[] args) {
-		System.out.println("git 테스트2222");
+		System.out.println("git 테스트");
 
 	}
 
