601dd8e57005923a3c435786f8c87a4f1d514048
Gubong_home <QAZ3915@NAVER.COM>
2024-01-17 22:50:54 +0900

    test 240117_22:50

--- src/Test/Exam.java
+++ src/Test/Exam.java
@@ -4,6 +4,8 @@ public class Exam {
 
 	public static void main(String[] args) {
 		System.out.println("git 테스트2222");
+		System.out.println("git 테스트2222");
+		System.out.println("git 테스트2222");
 
 	}
 
